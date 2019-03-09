73743a4c68158d2bb144f0bbf40e600847c3e450
yangjiayu <1761398181@qq.com>
2019-03-09 20:18:46 +0800

    plan of day

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
 (load-file "~/.emacs.d/lisp/init-packages.el")
 (load-file "~/.emacs.d/lisp/init-better-defaults.el")
 (load-file "~/.emacs.d/lisp/init-kbd.el")
+(load-file "~/.emacs.d/lisp/org.el")
 
 ;; Package Management
 ;; -----------------------------------------------------------------
@@ -23,6 +24,7 @@
  '(custom-safe-themes
    (quote
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+ '(org-agenda-files (quote ("~/.emacs.d/lisp/gtd.org")))
  '(package-selected-packages
    (quote
     (undo-tree company-c-headers company flycheck yasnippet auto-complete-c-headers racket-mode hungry-delete swiper counsel smartparens popwin helm helm-ag nyan-mode js2-mode nodejs-repl exec-path-from-shell xcscope monokai-theme spacemacs-theme))))
--- lisp/org.el
+++ lisp/org.el
@@ -4,6 +4,6 @@
 (require 'org)(setq org-src-fontify-natively t);;org语法高亮
 
 (setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "~/.emacs.d/gtd.org" "学习计划")
+      '(("t" "Todo" entry (file+headline "~/.emacs.d/lisp/gtd.org" "学习计划")
 	 "* TODO [#B] %?\n  %i\n"
 	 :empty-lines 1)))
